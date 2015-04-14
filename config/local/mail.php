24682ad905c46553abc11ad1efdfda2d1b56d5ee
midoupa3 <midoupa3@gmail.com>
2015-04-14 15:25:58 +0100

    0.1
    0.1

--- config/local/mail.php
+++ config/local/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => admin@kafilYatim.dz, 'name' => Mouloude],
+	'from' => ['address' => 'admin@kafilYatim.dz', 'name' => 'Mouloude'],
 
 	/*
 	|--------------------------------------------------------------------------
